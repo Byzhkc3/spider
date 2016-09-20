45d3b4e62b7ab169bdb39ae60cc65e7d4a8fca58
moyh <moyh>
2016-09-20 17:14:58 +0800

    忘了备注
    Signed-off-by: moyh <moyh>

--- 开发文档/表修改备注.sql
+++ 开发文档/表修改备注.sql
@@ -8,5 +8,6 @@ alter table t_shixin_invalid add index `search`(`err_type`, `flag`);
 
 # 备注
 # 目前无法对被执行人数据进行分类[公司/个人]
-
+alter table t_zhixing_valid add index `search`(`name`, `card_num`);
+alter table t_zhixing_invalid add index `search`(`err_type`, `flag`);
 # 运营商表通用一份
