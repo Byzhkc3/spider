bb2c2967834595bb09e05b1c4c828896414a3981
moyh <moyh>
2016-09-29 09:55:30 +0800

    修改了政府机构联络方式表

--- current/phone_book/additional/create_table/t_phone_book.sql
+++ current/phone_book/additional/create_table/t_phone_book.sql
@@ -7,13 +7,14 @@ create table `t_phone_book`(
     `alter_time` timestamp default current_timestamp on update current_timestamp,
 
 
-    `province` varchar(64) default null comment 'province',
+    `province` varchar(64) not null comment 'province',
     `city` varchar(64) default null comment 'city',
     `sub_city` varchar(64) default null comment '区',
     `town_street` varchar(64) default null comment 'town/street',
 
     `department` varchar(128) not null comment '机关称呼',
-    `tel_num` varchar(32) default null comment '电话号码'
+    `tel_num` varchar(32) not null comment '电话号码',
+ 	unique key `unique_key` (`province`,`city`,`sub_city`,`town_street`,`department`,`tel_num`)
 
 )engine = innodb default charset=utf8 auto_increment=1 comment='政府机关联络方式表';
 
