1fa59dae186af1ccee7e820b65f81c945af3ae34
moyh <moyh>
2016-10-09 18:00:03 +0800

    大修改 Signed-off-by: moyh <moyh>

--- 开发文档/表修改备注.sql
+++ 开发文档/表修改备注.sql
@@ -11,3 +11,8 @@ alter table t_shixin_invalid add index `search`(`err_type`, `flag`);
 alter table t_zhixing_valid add index `search`(`name`, `card_num`);
 alter table t_zhixing_invalid add index `search`(`err_type`, `flag`);
 # 运营商表通用一份
+
+
+#2016-10-09
+alter table t_shixin_valid drop index search;
+alter table t_shixin_valid add index `search`(`name`, `card_num`, `area_name`, `flag`);
