781b0d3ffb2fa77684a527c0007e62dc04fc8434
moyh <moyh>
2016-10-19 11:55:37 +0800

    # 引入了简版征信的包
    Signed-off-by: moyh <moyh>

--- flask_spider/spider/__init__.py
+++ flask_spider/spider/__init__.py
@@ -2,3 +2,4 @@ from shixin_spider import shixinSearchAPI
 from zhixing_spider import zhixingSearchAPI
 # from phone_book import phonebookSpiderAPI
 from operator_spider import getPhoneAttr, chinaUnicomAPI, getNoteCode, loginSys
+from credit_report import creditPersonAPI
