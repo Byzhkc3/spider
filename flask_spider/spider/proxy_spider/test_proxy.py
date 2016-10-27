b4abe17b5bfa286823ddadd94c7af64e08146b1d
moyh <moyh>
2016-10-27 09:26:39 +0800

    文件的整合 Signed-off-by: moyh <moyh>

--- flask_spider/spider/proxy_spider/test_proxy.py
+++ flask_spider/spider/proxy_spider/test_proxy.py
@@ -1,3 +1,4 @@
+import requests
 from requests.utils import dict_from_cookiejar
 from proxy_spider import  getVaildProxiesAPI
 
@@ -11,7 +12,7 @@ def testProxy(proxy):
 
     proxies = {'http':proxy, 'https':proxy}
     try:
-        response = get(url, proxies=proxies, timeout=3)
+        response = requests.get(url, proxies=proxies, timeout=3)
     except (ReadTimeout, ConnectTimeout, Timeout):
         pass
     except ConnectionError:
